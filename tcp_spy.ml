4acee2298d821628eb649580ac5fa2ba544c3d6a
Gorm Casper <casperin@users.noreply.github.com>
2020-06-30 14:25:17 +0200

    Refactor based on feedback

--- tcp_spy.ml
+++ tcp_spy.ml
@@ -1,50 +1,41 @@
 open Core
 open Async
 
-
-let rec pipe r w buffer dir =
-  Reader.read r buffer
-  >>= function
+let rec pipe r w prefix =
+  match%bind Reader.read_line r with
   | `Eof -> return ()
-  | `Ok bytes_read ->
-    let s = Bytes.sub buffer ~pos:0 ~len:bytes_read |> Bytes.to_string in
-    print_string @@ String.substr_replace_all s ~pattern:"\n" ~with_:("\n"^dir^" ");
-    Writer.write w s;
-    Writer.flushed w
-    >>= fun () ->
-    pipe r w buffer dir
-
-
+  | `Ok line ->
+    printf "%s %s\n" prefix line;
+    Writer.write w line;
+    Writer.write w "\n";
+    pipe r w prefix
 
 let run port (host, target) =
-  print_endline @@ "Listening on port "^string_of_int port^", forwarding to "^host^":"^string_of_int target;
-  Tcp.Server.create
-    ~on_handler_error:`Raise
-    (Tcp.Where_to_listen.of_port port)
-    (fun _addr r_client w_client ->
-      (* Make connection to server *)
-      Tcp.with_connection
+  printf "Listening on port %d, forwarding to %s:%d\n" port host target;
+  let%bind socket =
+    Tcp.Server.create
+      ~on_handler_error:`Raise
+      (Tcp.Where_to_listen.of_port port)
+      (fun _addr r_client w_client ->
+        (* Make connection to server *)
+        Tcp.with_connection
         (Tcp.Where_to_connect.of_host_and_port { host; port = target })
         (fun _addr r_server w_server ->
-          (* Set up piping between client and server *)
-          let buffer1 = Bytes.create (16 * 1024) in
-          let buffer2 = Bytes.create (16 * 1024) in
-          Deferred.any [
-            pipe r_client w_server buffer1 ">";
-            pipe r_server w_client buffer2 "<"
-          ]))
-  >>= Tcp.Server.close_finished 
-
-
+          let%bind () =
+            pipe r_client w_server "> "
+          and () =
+            pipe r_server w_client "< "
+          in
+          return ()
+        ))
+  in
+  Tcp.Server.close_finished socket
 
 let parse_target target =
-  String.split_on_chars target ~on:[':']
-  |> function
-    | [host; port] -> (host, int_of_string port)
-    | [port] -> ("localhost", int_of_string port)
-    | _ -> ("localhost", 8080)
-
-
+  String.split_on_chars target ~on:[':'] |> function
+  | [host; port] -> (host, int_of_string port)
+  | [port] -> ("localhost", int_of_string port)
+  | _ -> ("localhost", 8080)
 
 let () =
   Command.async
@@ -60,4 +51,3 @@ let () =
         ))
     end
   |> Command.run
-
